df01322f868fb08f24ca48026003537670ee6679
ArminNaish <armin.naish@outlook.com>
2021-01-10 15:58:45 +0100

    day4

--- AdventOfCode/day4/passport-processing.js
+++ AdventOfCode/day4/passport-processing.js
@@ -1,3 +1,13 @@
+const validator = {
+    'byr': validateBirthYear,
+    'iyr': validateIssueYear,
+    'eyr': validateExpirationYear,
+    'hgt': validateHeight,
+    'hcl': validateHairColor,
+    'ecl': validateEyeColor,
+    'pid': validatePassportId,
+};
+
 export function validate(sequence){
     const passport = parse(sequence);
     return verify(passport);
@@ -19,14 +29,67 @@ function verify(passport){
         if (!passport.hasOwnProperty(field) ) {
             return false;
         }
-        if (passport[field] == null){
+        if (passport[field] === null){
+            return false;
+        }
+        if (!validator[field](passport[field])){
             return false;
         }
     }
     return true;
 }
 
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+function validateBirthYear(value){
+    return isInRange(1920, 2002, value);
+}
 
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+function validateIssueYear(value){
+    return isInRange(2010, 2020, value);
+}
+
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+function validateExpirationYear(value){
+    return isInRange(2020, 2030, value);
+}
+
+// hgt (Height) - a number followed by either cm or in:
+//     If cm, the number must be at least 150 and at most 193.
+//     If in, the number must be at least 59 and at most 76.
+function validateHeight(value){
+    const regex = /(\d+)(.+)/i;
+    const [number, unit] = value.match(regex).slice(1,3);
+    return unit.toLowerCase() === 'cm'
+        ? isInRange(150, 193, number)
+        : isInRange(59,76, number);
+}
+
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+function validateHairColor(value){
+    const regex = /^#[0-9a-f{6}]+$/;
+    const match = value.match(regex);
+    return match !== null;
+}
+
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+function validateEyeColor(value){
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+}
+
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+function validatePassportId(value){
+    const regex = /^[0-9]{9}$/;
+    const match = value.match(regex);
+    return match !== null;
+}
+
+function isInRange(from, to, value){
+    var number = parseInt(value);
+    if (isNaN(number))
+        return false;
+    return (number >= from && number <= to)
+}
 
 
 const text = await Deno.readTextFile('./input.txt');
--- AdventOfCode/day4/passport-processing.tests.js
+++ AdventOfCode/day4/passport-processing.tests.js
@@ -1,4 +1,4 @@
-import { parse, validate } from './passport-processing.js';
+import { validate } from './passport-processing.js';
 import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
 Deno.test("valid: all fields are present", () => {
@@ -41,3 +41,84 @@ Deno.test("invalid: cid and byr is missing", () => {
     assertEquals(result, false);
 });
 
+
+
+// --- part 2 ---
+
+
+Deno.test("valid 1: all fields are present and valid", () => {
+    const passport =`
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+    `;
+    const result = validate(passport);
+    assertEquals(result, true);
+});
+
+Deno.test("valid 2: all fields are present and valid", () => {
+    const passport =`
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+    `;
+    const result = validate(passport);
+    assertEquals(result, true);
+});
+
+Deno.test("valid 3: all fields are present and valid", () => {
+    const passport =`
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+    `;
+    const result = validate(passport);
+    assertEquals(result, true);
+});
+
+Deno.test("valid 4: all fields are present and valid", () => {
+    const passport =`
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    `;
+    const result = validate(passport);
+    assertEquals(result, true);
+});
+
+
+Deno.test("invalid 1: all fields are present but not valid", () => {
+    const passport =`
+    eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+    `;
+    const result = validate(passport);
+    assertEquals(result, false);
+});
+
+
+Deno.test("invalid 2: all fields are present but not valid", () => {
+    const passport =`
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+    `;
+    const result = validate(passport);
+    assertEquals(result, false);
+});
+
+Deno.test("invalid 3: all fields are present but not valid", () => {
+    const passport =`
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+    `;
+    const result = validate(passport);
+    assertEquals(result, false);
+});
+
+Deno.test("invalid 4: all fields are present but not valid", () => {
+    const passport =`
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007
+    `;
+    const result = validate(passport);
+    assertEquals(result, false);
+});
